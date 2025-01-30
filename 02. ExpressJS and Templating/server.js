44960485c72acb22575c2d38d2b76afe61720c93
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-30 10:06:30 +0200

    Express routing

--- 02. ExpressJS and Templating/server.js
+++ 02. ExpressJS and Templating/server.js
@@ -8,8 +8,24 @@ app.use(express.static(`public`))
 app.engine('hbs', engine({ extname: '.hbs' }))
 app.set(`view engine`, `hbs`);
 
+// Routes
 app.get(`/`, (req, res) => {
     res.render(`home`);
-})
+});
+app.get(`/about`, (req, res) => {
+    res.render(`about`);
+});
+app.get(`/addMovie`, (req, res) => {
+    res.render(`newMovie`);
+});
+app.get(`/details`, (req, res) => {
+    res.render(`details`);
+});
+app.get(`/search`, (req, res) => {
+    res.render(`search`);
+});
+app.get(`/*`, (req, res) => {
+    res.render(`404`);
+});
 
 app.listen(port, console.log(`App runs on:\nhttp://localhost:${port}`));
