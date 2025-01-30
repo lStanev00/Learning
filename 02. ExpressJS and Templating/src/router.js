7a769307804c93c80d9821b2f4ff5e109b8b43ff
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-30 11:20:54 +0200

    Dir updates

--- 02. ExpressJS and Templating/src/router.js
+++ 02. ExpressJS and Templating/src/router.js
@@ -4,8 +4,10 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('home'));
 router.get('/about', (req, res) => res.render('about'));
 router.get('/addMovie', (req, res) => res.render('newMovie'));
-router.get('/details', (req, res) => res.render('details'));
 router.get('/search', (req, res) => res.render('search'));
+router.get('/details/:id', (req, res) => {
+    res.render('details')
+});
 
 // Handle not found request
 router.get('/*', (req, res) => res.status(404).render('404'));
