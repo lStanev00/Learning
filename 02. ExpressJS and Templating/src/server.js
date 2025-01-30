fd7bb43d72bda9cfe136f8b3938dc5e7bfe7f7e5
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-30 11:44:18 +0200

    Correct file linking after redir

--- 02. ExpressJS and Templating/src/server.js
+++ 02. ExpressJS and Templating/src/server.js
@@ -5,9 +5,10 @@ const routes = require(`./router.js`)
 const app = express();
 const port = 3000;
 
-app.use(express.static(`public`))
+app.use(express.static(`src/public`))
 app.engine('hbs', engine({ extname: '.hbs' }))
 app.set(`view engine`, `hbs`);
+app.set(`views`, `./src/views`)
 
 // Router invoke
 app.use(`/`, routes);
