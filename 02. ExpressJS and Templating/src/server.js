5e05c8ac4829a7f4b45855e039c53e35ef627935
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 15:02:02 +0200

    Add comments

--- 02. ExpressJS and Templating/src/server.js
+++ 02. ExpressJS and Templating/src/server.js
@@ -10,7 +10,7 @@ app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
-app.use(express.json()); 
+app.use(express.json()); // Parser for json incomming
 
 // Router invoke
 app.use("/", routes);
