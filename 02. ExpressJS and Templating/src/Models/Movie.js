23614483e97922e3eede809bcc6406a4a510ee08
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 10:03:02 +0200

    New mongo models init

---
+++ 02. ExpressJS and Templating/src/Models/Movie.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Movie = new mongoose.Schema({
+    title: {type: String, required: true},
+    category: {type: String, required: true},
+    genre: {type: String, required: true},
+    director: {type: String, required: true},
+    year: {type: Number, required: true, max: Date().getFullyear()},
+    rateing: {type: Number, required: true, min: 0, max: 10},
+    description: {type: String, required: true, maxlength: 200},
+    imgURL: {type: String, requered: true, validate: function(v){return /^(http|https):\/\/.+/.test(v);}},
+    cast: [{type: mongoose.Schema.Types.ObjectId, ref: "Cast"}]
+})
