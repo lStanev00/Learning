e6e8bfa688405ab76b9a0742ca5b99e0d9f3a737
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 17:01:29 +0200

    Error MSG display in alert window(know better practices but this is for education purpose)

--- 02. ExpressJS and Templating/src/public/handlers/addMovieHandler.js
+++ 02. ExpressJS and Templating/src/public/handlers/addMovieHandler.js
@@ -4,9 +4,20 @@ formEl.addEventListener(`submit`, async (e) => {
     e.preventDefault();
 
     const formData = new FormData(formEl);
-    let { category, description, director, genre, imageURL, rating, title, year } = Object.fromEntries(formData.entries());
+    let { category, description, director, genre, imgURL, rating, title, year } = Object.fromEntries(formData.entries());
     rating = Number(rating);
     year = Number(year);
+    console.log({
+        category: category,
+        description: description,
+        director: director,
+        genre: genre,
+        imgURL: imgURL,
+        rating: rating,
+        title: title,
+        year: year,
+    });
+    
 
     const req = await fetch(`http://localhost:3000/addMovie`, {
         headers: {
@@ -18,10 +29,25 @@ formEl.addEventListener(`submit`, async (e) => {
             description: description,
             director: director,
             genre: genre,
-            imageURL: imageURL,
+            imgURL: imgURL,
             rating: rating,
             title: title,
             year: year,
         }),
     });
+
+    if (req.status == 400) {
+        const data = await req.json()
+        window.alert(errorToMSG(data))
+        
+    }
 })
+
+function errorToMSG(arr){
+    let msgBuild = arr.shift();
+    if (arr.length < 1) return msgBuild
+    for (const error of arr) {
+        msgBuild += `\n` + error
+    };
+    return msgBuild
+}
