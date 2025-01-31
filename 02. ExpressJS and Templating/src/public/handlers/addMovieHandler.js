7cffcdea7e81900e4cff7fc815c643b812e1456b
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 15:03:05 +0200

    Posting the data correctly

--- 02. ExpressJS and Templating/src/public/handlers/addMovieHandler.js
+++ 02. ExpressJS and Templating/src/public/handlers/addMovieHandler.js
@@ -4,13 +4,24 @@ formEl.addEventListener(`submit`, async (e) => {
     e.preventDefault();
 
     const formData = new FormData(formEl);
-    console.log(Object.fromEntries(formData.entries()));
+    let { category, description, director, genre, imageURL, rating, title, year } = Object.fromEntries(formData.entries());
+    rating = Number(rating);
+    year = Number(year);
 
     const req = await fetch(`http://localhost:3000/addMovie`, {
         headers: {
             "Content-Type": "application/json"
         },
         method: "POST",
-        body: JSON.stringify(Object.fromEntries(formData.entries())),
+        body: JSON.stringify({
+            category: category,
+            description: description,
+            director: director,
+            genre: genre,
+            imageURL: imageURL,
+            rating: rating,
+            title: title,
+            year: year,
+        }),
     });
 })
