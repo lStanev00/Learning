1b7f3181377f8fa79f9a55e53d04c8d3e9e87df4
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 12:16:29 +0200

    Bug fixing

--- 02. ExpressJS and Templating/src/Models/Movie.js
+++ 02. ExpressJS and Templating/src/Models/Movie.js
@@ -5,9 +5,11 @@ const Movie = new mongoose.Schema({
     category: {type: String, required: true},
     genre: {type: String, required: true},
     director: {type: String, required: true},
-    year: {type: Number, required: true, max: Date().getFullyear()},
+    year: { type: Number, required: true, max: new Date().getFullYear(), min: 1888 },
     rateing: {type: Number, required: true, min: 0, max: 10},
     description: {type: String, required: true, maxlength: 200},
     imgURL: {type: String, requered: true, validate: function(v){return /^(http|https):\/\/.+/.test(v);}},
     cast: [{type: mongoose.Schema.Types.ObjectId, ref: "Cast"}]
-})
+});
+
+export default Movie
