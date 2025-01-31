0547a785283488d395a5293f2d1b007d971abd85
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 13:47:51 +0200

    The app now create movies in mongoDB

--- 02. ExpressJS and Templating/src/Models/Movie.js
+++ 02. ExpressJS and Templating/src/Models/Movie.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
-const Movie = new mongoose.Schema({
+const MovieSchema = new mongoose.Schema({
     title: {type: String, required: true},
     category: {type: String, required: true},
     genre: {type: String, required: true},
     director: {type: String, required: true},
     year: { type: Number, required: true, max: new Date().getFullYear(), min: 1888 },
-    rateing: {type: Number, required: true, min: 0, max: 10},
-    description: {type: String, required: true, maxlength: 200},
-    imgURL: {type: String, requered: true, validate: function(v){return /^(http|https):\/\/.+/.test(v);}},
+    rating: {type: Number, required: true, min: 0, max: 10},
+    description: {type: String, required: true, maxlength: 1000},
+    imgURL: {type: String, required: true, validate: function(v){return /^(http|https):\/\/.+/.test(v);}},
     cast: [{type: mongoose.Schema.Types.ObjectId, ref: "Cast"}]
 });
+
+const Movie = mongoose.model("Movie", MovieSchema);
 
 export default Movie
--- 02. ExpressJS and Templating/src/controllers/newMoviePost.js
+++ 02. ExpressJS and Templating/src/controllers/newMoviePost.js
@@ -1,6 +1,14 @@
-import express from "express";
-import mongoose from "mongoose";
+import Movie from "../Models/Movie.js";
 
 export default async function newMoviePost(req, res){
-    console.log(req.body);
+    req.body.year = Number(req.body.year);
+    req.body.rating = Number(req.body.rating);
+    try {
+        const newMovie = new Movie(req.body);
+        await newMovie.save();
+
+        res.status(201).json({ message: "Movie added successfully", data: newMovie });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 }
