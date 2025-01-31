4475eb3bbcd810acc4b1340d51f3c23727dba422
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 16:47:00 +0200

    The model now checks if the movie already existy before making a new movie in the database
    added validation error messages

--- 02. ExpressJS and Templating/src/Models/Movie.js
+++ 02. ExpressJS and Templating/src/Models/Movie.js
@@ -1,16 +1,45 @@
 import mongoose from "mongoose";
 
 const MovieSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    category: {type: String, required: true},
-    genre: {type: String, required: true},
-    director: {type: String, required: true},
-    year: { type: Number, required: true, max: new Date().getFullYear(), min: 1888 },
-    rating: {type: Number, required: true, min: 0, max: 10},
-    description: {type: String, required: true, maxlength: 1000},
-    imgURL: {type: String, required: true, validate: function(v){return /^(http|https):\/\/.+/.test(v);}},
-    cast: [{type: mongoose.Schema.Types.ObjectId, ref: "Cast"}]
+    title: { type: String, required: [true, "Title is required"] },
+    category: { type: String, required: [true, "Category is required"] },
+    genre: { type: String, required: [true, "Genre is required"] },
+    director: { type: String, required: [true, "Director is required"] },
+    year: { 
+        type: Number, 
+        required: [true, "Year is required"], 
+        max: [new Date().getFullYear(), "Year cannot be in the future"], 
+        min: [1888, "Year cannot be before 1888"] 
+    },
+    rating: { 
+        type: Number, 
+        required: [true, "Rating is required"], 
+        min: [0, "Rating must be at least 0"], 
+        max: [10, "Rating cannot be more than 10"] 
+    },
+    description: { type: String, required: [true, "Description is required"], maxlength: [1000, "Description is too long (max 1000 characters)"] },
+    imgURL: { 
+        type: String, 
+        required: [true, "Image URL is required"], 
+        validate: {
+            validator: function(v) {
+                return /^(http|https):\/\/.+/.test(v);
+            },
+            message: "Movie Poster invalid image URL format"
+        }
+    },
+    cast: [{ type: mongoose.Schema.Types.ObjectId, ref: "Cast" }]
 });
+
+MovieSchema.pre("save", async function (next) {// Check if the movie exist already
+    const exist = await mongoose.model("Movie").findOne({ title: this.title, year: this.year});
+
+    if (exist) {
+        return next(new Error("Movie already exists!"));
+    }
+
+    next();
+})
 
 const Movie = mongoose.model("Movie", MovieSchema);
 
