8d5c6e612b261641f438f32455f3a45c7302d88f
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-01-31 12:17:05 +0200

    DBconnect error handling

--- 02. ExpressJS and Templating/src/Helpers/mongoHelpers.js
+++ 02. ExpressJS and Templating/src/Helpers/mongoHelpers.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
-const uri = `mongodb://127.0.0.1:27017/`
+const uri = `mongodb://127.0.0.1:27017/movieMagic`
 
 export async function DBconnect() {
-    mongoose.connect(uri)
-}
+    try {
+        await mongoose.connect(uri);
+        console.log("MongoDB Connected Successfully!");
+    } catch (err) {
+        console.error("MongoDB Connection Error:", err.message);
+        process.exit(1); 
+    }
+};
+
