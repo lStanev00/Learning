c31fc5465e62b7499487afbddb6b0ddedc95cba9
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-01 15:04:02 +0200

    Temporari Cast model test adjustments

--- 02. ExpressJS and Templating/src/Models/Cast.js
+++ 02. ExpressJS and Templating/src/Models/Cast.js
@@ -4,8 +4,8 @@ const CastSchema = new mongoose.Schema({
     name: { type: String, required: [true, "Actor name is required"], trim: true },
     age: { type: Number, required: [true, "Age is required"], min: [0, "Age must be 0 or greater"], max: [110, "Age must be 110 or less"] },
     born: { type: String, required: [true, "Birthplace is required"], trim: true },
-    nameInMovie: { type: String, required: [true, "Character name in the movie is required"], trim: true },
-    castImgURL: { 
+    // nameInMovie: { type: String, required: [true, "Character name in the movie is required"], trim: true },
+    imgURL: { 
         type: String, 
         required: [true, "Actor Image URL is required"], 
         validate: {
@@ -20,12 +20,11 @@ const CastSchema = new mongoose.Schema({
 CastSchema.pre("save", async function (next) {
     const existingCast = await mongoose.model("Cast").findOne({ 
         name: this.name, 
-        nameInMovie: this.nameInMovie, 
-        movie: { $in: this.movie }
+        // movie: { $in: this.movie }
     });
 
     if (existingCast) {
-        return next(new Error(`Actor "${this.name}" already exists in this movie as "${this.nameInMovie}"!`));
+        return next(new Error(`Actor "${this.name}" already exists!`));
     }
     
     next();
