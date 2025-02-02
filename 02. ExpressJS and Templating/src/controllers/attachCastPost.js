e42cadd2e8238ca9e90e644a9fcc0083bf0d85e2
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-02 13:47:23 +0200

    Test logs deleted

--- 02. ExpressJS and Templating/src/controllers/attachCastPost.js
+++ 02. ExpressJS and Templating/src/controllers/attachCastPost.js
@@ -1,14 +1,11 @@
-import Cast from "../Models/Cast.js";
 import Movie from "../Models/Movie.js";
 
 export default async function attachCastPost(req, res) {
     const newCastId = req.body.cast;
     const movieID = req.params.id;
-    console.log(movieID);
     
     const movie = await Movie.findById(movieID);
     movie.cast.push(newCastId);
-    console.log(movie);
     await movie.save();
     
 }
