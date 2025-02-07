4cd9a6a504cc2f4cec1dbea5f8148ba2d4eb365e
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-07 15:30:35 +0200

    Helper for token creating

---
+++ 02. ExpressJS and Templating/src/Helpers/jwtHelpers.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+// import { verify } from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET
+
+export function generateToken(user) { 
+    return jwt.sign({
+        _id : user._id,
+        email: user.email,
+        // deviceInfo: user.deviceInfo,
+    }, SECRET, { expiresIn: "10d" })
+}
+
+export function verifyToken(token) {
+    try {
+        return jwt.verify(token, SECRET)
+    } catch (error) {
+        return null
+    }
+}
