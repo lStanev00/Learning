fd3a971d92e213a5dc413d5358e390ec741e881a
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-07 12:32:34 +0200

    Password now hashed in the Model

--- 02. ExpressJS and Templating/src/Models/User.js
+++ 02. ExpressJS and Templating/src/Models/User.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
+import { encrypt } from "../Helpers/bcryptHelpers.js";
 
 const userSchema = new mongoose.Schema({
-    email : { type: String, require: [true, "Email is required"] },
+    email : { type: String, require: [true, "Email is required"], unique: true },
     password : { type: String, require: [true, "Password is required"] },
+    sessionID: { type: String, default: ""},
 })
 
 userSchema.pre("save", async function (next) {
     try {
-        const exist = await mongoose.model(`User`).findOne({ email: this.email });
-
-        if (exist) {
-            return next(new Error(`The email : ${exist.email} already exist!`))
+        if (this.isModified("password")) { // Hash the passw only if moded
+            this.password = await encrypt(this.password);
         }
-
         next();
     } catch (error) {
         next(error);
--- 02. ExpressJS and Templating/src/controllers/registerPost.js
+++ 02. ExpressJS and Templating/src/controllers/registerPost.js
@@ -1,13 +1,12 @@
 import User from "../Models/User.js";
-import { encrypt } from "../Helpers/bcryptHelpers.js";
 
 export async function registerPost(req, res) {
     const { email, password, rePass} = req.body;
 
     if (password == rePass){
-        const newUser = new User({  email: email, password: await encrypt(password)   });
+        const newUser = new User({  email: email, password: password  });
         newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
+        res.redirect(`/register`);
     }else {
         return res.status(400).json({ message: "Passwords do not match" });
     }
