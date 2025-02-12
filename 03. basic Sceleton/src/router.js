086a502a3d0b85705cddaca18b6a9f6f90297e67
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-12 13:09:19 +0200

    Router now working with controllers

--- 03. basic Sceleton/src/router.js
+++ 03. basic Sceleton/src/router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import homeController from "./controllers/homeController.js";
 
 const router = Router();
 
-router.get(`/`, (req, res) => res.send(`Hello world`))
+// router.get(`/`, (req, res) => res.send(`Hello world`));
+router.use(`/`, homeController)
 
 export default router
