3ec74943ac078e82d372223e86d3b73fae4a3219
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-14 07:49:12 +0200

    Login controller

---
+++ 03. basic Sceleton/src/controllers/loginController.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import User from '../Models/User.js';
+import { generateToken } from '../Helpers/jwtHelpers.js';
+import { encrypt, validatePassword } from "../Helpers/bcryptHelpers.js";
+
+const loginControleer = Router();
+
+loginControleer.get(`/login`, loadPage);
+
+loginControleer.post(`/login`, loginAttempt);
+
+function loadPage(req,res) {
+    res.render(`/login`);
+}
+
+async function loginAttempt(req, res) {
+    const { email, passwoard } = req.body; // Username?
+
+    console.log( email );
+
+    
+    try {
+        const attemptedUser = User.findOne({ email: email });
+        const actualPassword = attemptedUser.passwoard;
+
+        const validate = validatePassword(passwoard, actualPassword);
+
+        if (validate) {
+            const token = generateToken(attemptedUser);
+            res.cookie(`auth`, token, { httpOnly: true });
+            return res.redirect(`/`);
+        } else {
+            throw new Error(`Invalid input`);
+        }
+        
+    } catch (error) {
+        res.status(400).json({
+            error: error
+        })
+    }
+    
+}
